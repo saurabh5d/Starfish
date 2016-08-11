8a3641bf5466a64983c41ae8471c397bf0c9ff8b
VibrantVipin <vipin12457@gmail.com>
2016-08-11 20:48:36 +0530

    dhkjd

--- Documents/Visual Studio 2015/Projects/StarfishProject/StarfishProject/Controllers/HomeController.cs
+++ Documents/Visual Studio 2015/Projects/StarfishProject/StarfishProject/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace StarfishProject.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page. This is a test vipin-saurabh!";
+            ViewBag.Message = "Your application description page. This is a test !";
             return View();
         }
 
