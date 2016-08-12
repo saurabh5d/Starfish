1713108861ee02d7e0c4587e9edd711f8bb6fd23
VibrantVipin <vipin12457@gmail.com>
2016-08-12 10:48:24 +0530

    start

--- Documents/Visual Studio 2015/Projects/StarfishProject/StarfishProject/Controllers/HomeController.cs
+++ Documents/Visual Studio 2015/Projects/StarfishProject/StarfishProject/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace StarfishProject.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page. This is a test dghjdsjd!";
+            ViewBag.Message = "Your application description page. This is a test!";
             return View();
         }
 
